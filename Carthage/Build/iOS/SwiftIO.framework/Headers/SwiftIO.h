f4f3d34f636b9c2355c728b2262501b22a08c425
Jonathan Wight <jwight@mac.com>
2015-09-16 10:33:44 -0700

    Bake in iOS & Mac dependencies.

---
+++ Carthage/Build/iOS/SwiftIO.framework/Headers/SwiftIO.h
@@ -0,0 +1,19 @@
+//
+//  SwiftIO.h
+//  SwiftIO
+//
+//  Created by Jonathan Wight on 5/21/15.
+//  Copyright (c) 2015 schwa.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftIO.
+FOUNDATION_EXPORT double SwiftIOVersionNumber;
+
+//! Project version string for SwiftIO.
+FOUNDATION_EXPORT const unsigned char SwiftIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftIO/PublicHeader.h>
+
+
